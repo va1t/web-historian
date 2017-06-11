d2845c82af4858d96386f7456ca18c0091002605
valt <valt@valts-MBP.T-mobile.com>
2017-06-11 11:56:10 -0400

    working version

--- workers/htmlfetcher.js
+++ workers/htmlfetcher.js
@@ -1,2 +1,14 @@
+var helper = require('../helpers/archive-helpers');
+
+var fetcher = () => {
+  helper.readListOfUrls( (urls) => {
+    //removes  empty string from end of urls list
+    console.log ('FETCHING URLS')
+    urls.pop();
+    helper.downloadUrls(urls);
+  });
+};
+
 // Use the code in `archive-helpers.js` to actually download the urls
 // that are waiting.
+fetcher();
